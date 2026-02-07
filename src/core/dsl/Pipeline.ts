5f55e65412306d76958a72cea56c922c1e9fe7fb
Pascal Martineau <pascal@lewebsimple.ca>
2026-02-06 23:22:07 -0500

    feat: PipelineContext

--- src/core/dsl/Pipeline.ts
+++ src/core/dsl/Pipeline.ts
@@ -1,11 +1,27 @@
 import * as z from "zod";
 
+import { KvAdapter, QueueAdapter } from "../../adapters";
 import { driverNameSchema } from "../../types";
+import { Item } from "../Item";
+import { Source } from "../Source";
 
 import { stepSchema } from "./Steps";
+import { Task } from "./Task";
 
 export const pipelineSchema = z.object({
   driver: driverNameSchema,
   steps: z.array(stepSchema),
 });
 export type Pipeline = z.infer<typeof pipelineSchema>;
+
+export interface PipelineContext<TEntity> {
+  readonly source: Source<TEntity>;
+  readonly task: Task;
+  readonly pipeline: Pipeline;
+  readonly document: Document;
+  readonly adapters: {
+    queue: QueueAdapter<Task>;
+    kv?: KvAdapter;
+  };
+  item?: Item;
+}
